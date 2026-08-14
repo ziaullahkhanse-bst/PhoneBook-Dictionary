537666d87f3ce0ca7c580e87d8b76814228df70d
Ziaullah Khan <ziaullahkhan.se@gmail.com>
2026-08-14 20:57:28 +0500

    Delete args(sum).py

--- args(sum).py
+++
@@ -1,9 +0,0 @@
-def sum_all(*args):
-    total = 0
-    for num in args:
-        total = total + num
-    return total
-
-print(sum_all(1, 2, 3))
-print(sum_all(10, 20, 30, 40, 50))
-print(sum_all(5, 15))
